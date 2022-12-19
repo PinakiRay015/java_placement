a873c8be924fc4f08ccec6a1126341287a4085ca
RohanRay015 <pinaki.bpd2001@gmail.com>
2022-12-19 12:45:23 +0530

    java practices

---
+++ src/pattern/square_pattern2.java
@@ -0,0 +1,11 @@
+//this is another way to print square pattern
+package pattern;
+
+public class square_pattern2 {
+    public static void main(String[] args) {
+        for(int i=1 ; i<=4 ; i++)
+        {
+            System.out.println("****");
+        }
+    }
+}
