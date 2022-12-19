cb25390b464c2c9cb4ee9b6f5685fb424df18300
RohanRay015 <pinaki.bpd2001@gmail.com>
2022-12-19 13:09:21 +0530

    java practices

--- src/loops/problems/prob2.java
+++ src/loops/problems/prob2.java
@@ -1,4 +1,19 @@
+//java program to print the number in reversing order
 package loops.problems;
-
+import java.util.Scanner;
 public class prob2 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+
+        while(num!=0)
+        {
+            int lastdigit = num%10;
+            num =num/10;
+            System.out.print(lastdigit);
+
+        }
+    }
 }
