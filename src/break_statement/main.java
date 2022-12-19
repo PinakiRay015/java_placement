038e0f81287539166929346a71b25fe1d0448866
RohanRay015 <pinaki.bpd2001@gmail.com>
2022-12-20 00:47:07 +0530

    java practices

--- src/break_statement/main.java
+++ src/break_statement/main.java
@@ -1,13 +1,14 @@
+//implementing break statement to come out from the loop
 package break_statement;
 
 public class main {
     public static void main(String[] args)
     {
-        for(int i=1 ; i<=5 ; i++)
+        for(int i=1 ; i<=5 ; i++) //here the condition is to print 1-5 numbers
         {
             if(i==3)
             {
-                break;
+                break; //here the condition is to break the loop once I reach 3
             }
             System.out.println(i);
         }
