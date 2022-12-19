8d49beb91db853b644b6912a2435d0c83cce6fdb
RohanRay015 <pinaki.bpd2001@gmail.com>
2022-12-20 01:31:51 +0530

    java practices

---
+++ src/loops/problems/prob4.java
@@ -0,0 +1,19 @@
+//java program to enter all the digits entered by the user except the multiples of 10
+package loops.problems;
+import java.util.Scanner;
+public class prob4 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Enter the number");
+            int num = sc.nextInt();
+            if(num%10==0)
+            {
+                continue;
+            }
+            System.out.println("number is "+num);
+        }
+    }
+}
