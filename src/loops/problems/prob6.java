46b0ad0515bc540e74cb7477b9bda160bb65ab6a
RohanRay015 <pinaki.bpd2001@gmail.com>
2022-12-20 11:25:01 +0530

    java practices

---
+++ src/loops/problems/prob6.java
@@ -0,0 +1,18 @@
+//java program to find the factorial of a number
+package loops.problems;
+
+import java.util.Scanner;
+
+public class prob6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fact = 1;
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        for(int i=1 ; i<=num ; i++)
+        {
+            fact *= i;
+        }
+        System.out.println("Factorial of the number "+num+" is "+fact);
+    }
+}
---
+++ src/loops/problems/prob7.java
@@ -0,0 +1,16 @@
+//java program to print the multiplication table by taking the input from the user
+package loops.problems;
+
+import java.util.Scanner;
+
+public class prob7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the num");
+        int num = sc.nextInt();
+        for(int i=1 ; i<=10 ; i++)
+        {
+            System.out.println(num+"X"+i+"="+num*i);
+        }
+    }
+}
