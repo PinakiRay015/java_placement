024b571bdc2e4b2c56377777fb92830c01c7d6e1
RohanRay015 <pinaki.bpd2001@gmail.com>
2022-12-20 11:07:33 +0530

    java practices

---
+++ src/loops/problems/prob5.java
@@ -0,0 +1,30 @@
+//java program to print the sum of odd integers and positive integers
+package loops.problems;
+import java.util.Scanner;
+public class prob5 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int choice;
+        int psum = 0;
+        int nsum = 0;
+        do
+        {
+            System.out.println("Enter the number");
+            num = sc.nextInt();
+            if(num%2==0)
+            {
+                psum = psum+num;
+            }
+            else
+            {
+                nsum = nsum+num;
+            }
+            System.out.println("Do you want to enter more numbers? Press 1 to continue or 0 to end");
+            choice = sc.nextInt();
+        }while (choice==1);
+        System.out.println("Sum of the odd numbers are "+nsum);
+        System.out.println("Sum of the positive numbers are "+psum);
+    }
+}
