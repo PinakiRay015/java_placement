e893d6a30f3f044bf010799880b6526268e77514
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-01-06 02:07:00 +0530

    java practice

--- src/oneD_Array/arrayInitialize.java
+++ src/oneD_Array/arrayInitialize.java
@@ -1,32 +1,9 @@
 package oneD_Array;
-import java.util.Scanner;
-
 public class arrayInitialize {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
         //method 1
-//        int []array1 = {10 , 20 , 30 , 40 , 50};
-//        System.out.println("Here is your array");
-//        for(int i = 0 ; i< array1.length ; i++)
-//        {
-//            System.out.println(array1[i]);
-//        }
-//
-        //method 2
-        System.out.println("Enter the size of the array");
-        int size = sc.nextInt();
-        int [] array2 = new int[size];
-        for(int i=0 ; i< array2.length ; i++)
-        {
-            array2[i] = sc.nextInt();
-        }
-        System.out.println("Here is your array");
-        for(int i = 0 ; i< array2.length ; i++)
-        {
-            System.out.println(array2[i]);
-        }
+        int[] array1 = {10, 20, 30, 40, 50};
 
-        sc.close();
-    }
+        //method 2
+    }    int [] array2 = new int[50]; //here 50 is the size of the array
 }
---
+++ src/oneD_Array/arrayPart1.java
@@ -0,0 +1,20 @@
+package oneD_Array;
+
+import java.util.Scanner;
+
+public class arrayPart1 {
+    public static void main(String[] args) {
+        int[] marks = new int[100];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the marks of physics");
+        marks[0] = sc.nextInt();
+        System.out.println("Enter the marks of chemistry");
+        marks[1] = sc.nextInt();
+        System.out.println("Enter the marks of mathematics");
+        marks[2] = sc.nextInt();
+
+        System.out.println("Your marks in chemistry is "+marks[1]);
+        System.out.println("Your marks in physics is "+marks[0]);
+        System.out.println("Your marks in mathematics is "+marks[2]);
+    }
+}
