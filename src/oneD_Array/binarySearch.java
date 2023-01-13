0a20f35417dafc72b951190f4632fae919073680
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-01-13 08:24:04 +0530

    new changes

--- src/oneD_Array/binarySearch.java
+++ src/oneD_Array/binarySearch.java
@@ -1,8 +1,10 @@
-//java program to perform binary search in a sorted array
+//java program to perform binary search in an array
 package oneD_Array;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class binarySearch {
-    public static int binarySearch(int []array , int key)
+    public static int binSearch(int []array , int key)
     {
         int start = 0;
         int end = array.length-1;
@@ -13,36 +15,43 @@ public class binarySearch {
             {
                 return mid;
             }
-            else if(array[mid]>key)
+            else if(array[mid]<key)
             {
-                end = mid-1;
+                start = mid+1;
             }
             else
             {
-                start = mid+1;
+                end = mid-1;
             }
         }
         return -1;
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int []array = {2 , 4 , 6 , 8 , 10 , 12 , 14};
-        System.out.println("Here is your sorted array");
-        for(int i = 0 ; i < array.length ; i++)
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+        int []array = new int[size];
+        System.out.println("Enter the elements inside the array");
+        for(int i = 0 ; i<array.length ; i++)
+        {
+            array[i] = sc.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.println("Here is your array in sorted order");
+        for(int i =0; i<array.length ; i++)
         {
             System.out.print(array[i]+"\t");
         }
         System.out.println("\nEnter the key");
         int key = sc.nextInt();
-        int position = binarySearch(array , key);
+        int position = binSearch(array , key);
         if(position==-1)
         {
             System.out.println("Not found");
         }
         else
         {
-            System.out.println("The element is found in the index "+position);
+            System.out.println("Your element is in index position "+position);
         }
     }
 }
