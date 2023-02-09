cfa49dcdb01f7d35560b1ae339f8875805876884
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-10 03:12:29 +0530

    new changes

---
+++ src/sorting/problem/sortingInDecrease.java
@@ -0,0 +1,36 @@
+//java program to perform binary sort , selection sort , counting sort in decrease order
+package sorting.problem;
+
+public class sortingInDecrease {
+    public static void bubbleSort(int []array)
+    {
+        for(int turn = 0 ; turn<array.length-1 ; turn++)
+        {
+            for(int j = 0 ; j<array.length-1-turn ; j++)
+            {
+                if(array[j]<array[j+1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void display(int []array)
+    {
+        for(int i=0 ; i<array.length ; i++)
+        {
+            System.out.print(array[i]+"\t");
+        }
+    }
+    public static void main(String [] args)
+    {
+        int []array = {3 , 6 , 2 , 1 , 8 , 7 , 4 , 5 , 3 , 1};
+        display(array);
+        bubbleSort(array);
+        System.out.println("The array sorted in bubble sort 👇\n");
+        display(array);
+    }
+}
