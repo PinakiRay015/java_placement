72e94d463bab84234c57701aa16ef2dcb8dafd5e
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-11 12:38:19 +0530

    new changes

---
+++ src/TwoD_Array/prob1.java
@@ -0,0 +1,31 @@
+//java program to implement two dimensional array(matrix)
+package TwoD_Array;
+
+import java.util.Scanner;
+
+public class prob1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [][]matrix = new int[3][3];
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            for(int j=0 ; j<m ; j++)
+            {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0 ; i<n ; i++)
+        {
+            for(int j=0 ; j<m ; j++)
+            {
+                System.out.print(matrix[i][j]+"\t");
+            }
+
+            System.out.println("");
+        }
+    }
+}
