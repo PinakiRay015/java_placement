0b84a7c7062a323db7df2ddb5499f2fe302d8a74
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-28 19:19:46 +0530

    new changes

---
+++ src/String/Implementation.java
@@ -0,0 +1,20 @@
+//Java program to implement a String
+package String;
+
+import java.util.Scanner;
+
+public class Implementation {
+    public static void main(String [] args)
+    {
+        String str1 = "Pinaki"; //using default method
+
+        String str2 = new String("Rohan"); //using new keyword
+
+        Scanner sc = new Scanner(System.in); //using Scanner class by taking input from keyboard
+        System.out.println("Enter the name");
+        String str3 = sc.nextLine();
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
---
+++ src/String/stringFunctions.java
@@ -0,0 +1,8 @@
+package String;
+
+public class stringFunctions {
+    public static void main(String [] args)
+    {
+        String str = new String("Pinaki");
+    }
+}
