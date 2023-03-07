1786c163e829be768ddf17343cd2f177d727f150
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-08 02:04:53 +0530

    Java Sub-String Function

---
+++ src/String/subString.java
@@ -0,0 +1,10 @@
+//java program to print the substring of the String from starting index to ending index
+package String;
+
+public class subString {
+    public static void main(String[] args)
+    {
+        String str = new String("HelloWorld");
+        System.out.println(str.substring(0 , 5));
+    }
+}
