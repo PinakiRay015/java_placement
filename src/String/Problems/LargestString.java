f46e04199807ee5eb3356a67edf28de22d40c61c
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-08 10:26:19 +0530

    Largest String

---
+++ src/String/Problems/LargestString.java
@@ -0,0 +1,19 @@
+//java program to find the largest string
+package String.Problems;
+
+public class LargestString {
+    public static void main(String[] args)
+    {
+        String []fruits = {"Apple" , "Mango" , "Apple"};
+        String largest = fruits[0];
+        for(int i = 1 ; i<fruits.length ; i++)
+        {
+            if(largest.compareTo(fruits[i]) < 0)
+            {
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
+    }
+}
