a1c2db4b05f29b62c83307b303b60569b796f758
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-23 10:50:33 +0530

    binary AND

---
+++ src/BIT_MANIPULATION/bitwiseAnd.java
@@ -0,0 +1,8 @@
+//java program to illustrate the bitwise AND operator
+package BIT_MANIPULATION;
+
+public class bitwiseAnd {
+    public static void main(String[] args) {
+        System.out.println((5 & 6));
+    }
+}
