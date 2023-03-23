60fb016df773939d385c80475a5c1b5d8a645596
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-23 10:52:15 +0530

    binary OR

---
+++ src/BIT_MANIPULATION/bitwiseOR.java
@@ -0,0 +1,8 @@
+//java program to illustrate bitwise OR operator
+package BIT_MANIPULATION;
+public class bitwiseOR {
+    public static void main(String[] args)
+    {
+        System.out.println((5 | 6));
+    }
+}
