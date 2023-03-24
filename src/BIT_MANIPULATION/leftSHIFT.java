4f315a1aefc180cc225b72a1e13bc18581930c23
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-24 10:30:06 +0530

    binary left shift

---
+++ src/BIT_MANIPULATION/leftSHIFT.java
@@ -0,0 +1,8 @@
+//java program to illustrate the binary left shift
+package BIT_MANIPULATION;
+
+public class leftSHIFT {
+    public static void main(String[] args) {
+        System.out.println((5<<2));
+    }
+}
