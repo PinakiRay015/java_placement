ca31210acba795a5bdc10d4ae33db67e2ef36b81
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-24 10:02:15 +0530

    binary XOR

---
+++ src/BIT_MANIPULATION/bitwiseXOR.java
@@ -0,0 +1,8 @@
+//java program to illustrate the bitwise XOR
+package BIT_MANIPULATION;
+
+public class bitwiseXOR {
+    public static void main(String[] args) {
+        System.out.println(~5);
+    }
+}
