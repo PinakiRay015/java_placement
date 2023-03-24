79e954bb8f7dc08ba7386ce568fbb13a318e9e28
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-24 10:40:41 +0530

    binary right shift

---
+++ src/BIT_MANIPULATION/rightSHIFT.java
@@ -0,0 +1,8 @@
+//java program to illustrate the binary right shift
+package BIT_MANIPULATION;
+
+public class rightSHIFT {
+    public static void main(String[] args) {
+        System.out.println((6>>1));
+    }
+}
