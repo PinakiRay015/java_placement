7b459076b5f7028ea56fe9e2c946f13a73f5efcb
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-04-06 10:20:36 +0530

    constructor

---
+++ src/OOP/constructors.java
@@ -0,0 +1,19 @@
+//java program to illustrate custom constructor
+package OOP;
+
+public class constructors {
+    public static void main(String[] args) {
+        myStudent s1 = new myStudent("Pinaki");
+        System.out.println("The name of the student is "+s1.name);
+    }
+}
+
+class myStudent
+{
+    String name;
+    int roll;
+    public myStudent(String name)
+    {
+        this.name = name;
+    }
+}
