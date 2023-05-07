a58796f9991989300aa91268604717ea02616cb7
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-05-07 08:07:17 +0530

    copy constructor

--- src/OOP/constructor/copyConstructor1.java
+++ src/OOP/constructor/copyConstructor1.java
@@ -1,6 +1,6 @@
 package OOP.constructor;
 
-public class copyConstructor {
+public class copyConstructor1 {
     public static void main(String[] args) {
         student s1 = new student();
         student s2 = new student(s1);
