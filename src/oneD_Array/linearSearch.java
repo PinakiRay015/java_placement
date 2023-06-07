66eb795dedb77b9fae0ae0065429796b9a3d16bc
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-06-07 19:38:20 +0530

    Linear Search

--- src/oneD_Array/linearSearch.java
+++ src/oneD_Array/linearSearch.java
@@ -2,7 +2,7 @@
 package oneD_Array;
 
 public class linearSearch {
-    public static int mySearch(int [] array , int key)
+    public static int myLinearSearch(int [] array , int key)
     {
         for(int i=0 ; i<array.length ; i++)
         {
@@ -17,7 +17,7 @@ public class linearSearch {
     public static void main(String[] args) {
         int []array = {2,4,6,8,9,10,12,14};
         int key = 10;
-        int position = mySearch(array , key);
+        int position = myLinearSearch(array , key);
         System.out.println((position == -1) ? "Not found!!" : "Element is present on the index "+position);
     }
 }
