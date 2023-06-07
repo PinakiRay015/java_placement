cf12459aad8fd924edd3d90327f359e12ebca51a
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-06-07 10:32:08 +0530

    linear search

--- src/oneD_Array/arrayInitialize.java
+++
@@ -1,9 +0,0 @@
-package oneD_Array;
-public class arrayInitialize {
-    public static void main(String[] args) {
-        //method 1
-        int[] array1 = {10, 20, 30, 40, 50};
-
-        //method 2
-    }    int [] array2 = new int[50]; //here 50 is the size of the array
-}
--- src/oneD_Array/linearSearch.java
+++ src/oneD_Array/linearSearch.java
@@ -1,37 +1,8 @@
-//java program to perform linear search in an one dimensional array
 package oneD_Array;
 
-import java.util.Scanner;
 public class linearSearch {
-    public static int mySearch(int []numbers , int key)
+    public static void main(String args)
     {
-        for(int i =0 ; i<numbers.length ; i++)
-        {
-            if(key == numbers[i])
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int []array = {10 , 56 , 73 , 67 , 12};
-        System.out.println("Here is ur array");
-        for(int i =0 ; i<array.length ; i++)
-        {
-            System.out.print(array[i]+"\t");
-        }
-        System.out.println("\nEnter the key");
-        int key = sc.nextInt();
-        int position = mySearch(array , key);
-        if(position==-1)
-        {
-            System.out.println("Not found");
-        }
-        else
-        {
-            System.out.println("The element is present5 at the index position "+position);
-        }
+
     }
 }
