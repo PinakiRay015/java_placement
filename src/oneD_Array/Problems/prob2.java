659ebe0543d345b8ea28f8f6beefa6c6fb86fcca
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-06-13 10:33:01 +0530

    Reverse an Array

---
+++ src/oneD_Array/Problems/prob2.java
@@ -0,0 +1,27 @@
+package oneD_Array.Problems;
+
+import java.util.Arrays;
+
+public class prob2 {
+    public static int [] reverseArray(int []array)
+    {
+        int start = 0;
+        int last = array.length-1;
+
+        while(start<last)
+        {
+            int temp = array[start];
+            array[start] = array[last];
+            array[last] = temp;
+
+            start++;
+            last--;
+        }
+
+        return array;
+    }
+    public static void main(String[] args) {
+        int [] array = {2,4,6,8,10};
+        System.out.println(Arrays.toString(reverseArray(array)));
+    }
+}
