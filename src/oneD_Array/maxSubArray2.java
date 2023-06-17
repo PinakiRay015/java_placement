2e51c696ca12d1eef9f6976ec682e2f0575cd605
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-06-18 00:43:24 +0530

    max SubArray sum (prefix sum)

--- src/oneD_Array/maxSubArray2.java
+++ src/oneD_Array/maxSubArray2.java
@@ -1,25 +1,25 @@
-//java program to find the max subArray sum by prefix-Sum method
+//java program to find the maximum subArray sum using prefix array method
 package oneD_Array;
 
 public class maxSubArray2 {
     public static int myMax2(int []array)
     {
-        int currSum = 0;
+        int currentSum = 0;
         int maxSum = Integer.MIN_VALUE;
         int []prefixArray = new int[array.length];
         prefixArray[0] = array[0];
-        for(int i=1 ; i<prefixArray.length ; i++)
+        for(int i=1 ; i<array.length ; i++)
         {
             prefixArray[i] = prefixArray[i-1] + array[i];
         }
-        for(int i=0 ; i<array.length ; i++)
-        {
-            for(int j=i+1 ; j<array.length ; j++)
-            {
-                currSum = prefixArray[j] - prefixArray[i+1];
-            }
 
-            maxSum = Math.max(currSum , maxSum);
+        for(int start = 0 ; start<array.length ; start++)
+        {
+            for(int end = start ; end<array.length ; end++)
+            {
+                currentSum = (start == 0 ) ? prefixArray[end] : prefixArray[end] - prefixArray[start-1];
+                maxSum = Math.max(currentSum , maxSum);
+            }
         }
 
         return maxSum;
