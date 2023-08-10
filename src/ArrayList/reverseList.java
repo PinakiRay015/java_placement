ed6721cdfb7a2c4c04e9abcc300dc33fba2918e1
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-08-10 07:33:02 +0530

    revering an ArrayList

---
+++ src/ArrayList/reverseList.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class reverseList {
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.println(list);
+
+        Collections.reverse(list);
+
+        System.out.println(list);
+    }
+}
