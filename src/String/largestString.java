9650aa6923214553b667fbd9bb2bbe05e7ea91aa
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-08-25 13:21:15 +0530

    largest string

---
+++ src/String/largestString.java
@@ -0,0 +1,18 @@
+//java program to find the largest string according to lexicographical order
+package String;
+
+public class largestString {
+    public static void main(String[] args) {
+        String [] fruits = {"apple" , "watermelon" , "pineapple"};
+        String largest = fruits[0];
+        for(int i=1 ; i<fruits.length ; i++)
+        {
+            if(largest.compareTo(fruits[i]) < 0)
+            {
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
+    }
+}
