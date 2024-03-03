ca345c60ca8b465741b9149a9006104083919ead
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-03 11:52:07 +0530

    add element at first

---
+++ src/LinkedList/Sample1.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+public class Sample1 {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+        public static Node head;
+        public static Node tail;
+
+        public void addFirst(int data)
+        {
+            Node newNode = new Node(data);
+            if(head==null)
+            {
+                head=tail=newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+    public static void main(String[] args) {
+        Sample1 s1 = new Sample1();
+        s1.addFirst(1);
+        s1.addFirst(2);
+    }
+}
